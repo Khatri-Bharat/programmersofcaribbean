205c4ab0a73726a059d6cf6589217448920af890
Khatri-Bharat <khatrimonty@gmail.com>
2014-01-07 03:13:16 +0530

    Fixed express code, CSS files being rendered properly

--- controller/web.js
+++ controller/web.js
@@ -1,15 +1,21 @@
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
+var ejs = require("ejs");
+var fs = require("fs");
+
 var app = express();
 
 app.use(logfmt.requestLogger());
 
+app.configure(function() {
+	app.engine('html', ejs.renderFile);
+	app.use('/resources', express.static(__dirname + "/../resources"));
+});
+
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+	res.render("../view/templates/landing.html");
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.listen(port);
