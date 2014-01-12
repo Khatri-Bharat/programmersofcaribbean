880e028f06d40bf28c2d68d666ff220308a585d4
Khatri-Bharat <khatrimonty@gmail.com>
2014-01-12 15:39:12 +0530

    Initial commit for server-side code of sir-trevor-js

---
+++ resources/js/sir-trevor-renderer.js
@@ -0,0 +1,25 @@
+(function(){
+	var root = this;
+	var SirTrevorRenderer = root.SirTrevorRenderer = {};
+	SirTrevorRenderer.render = function(json) {
+		var partials = _.map(json.data, SirTrevorRenderer.renderPartial(blockJson)); 	
+		var html = partials.join("\n");
+	};
+
+	SirTrevorRenderer.renderPartial = function(blockJson) {
+		var type = blockJson.type;
+		var data = blockJson.data;
+		var text = data.text;
+		var rep = "";
+		switch(type) {
+			case "text":
+				rep += "<p>" + text + "</p>";			
+				break;
+			case "heading":
+				rep += "<h2><b>" + text + "</b></h2>";
+				break;
+			default:	
+		}
+		return rep;
+	};
+})();
